0e93172b3a7902be9acae30e5e9ac7569e0822c2
The.Real.Slim.Tony <antoniomendezrojascr@gmail.com>
2024-04-29 21:04:15 -0600

    Fix typo in .gitignore and add cinema name to movie scrapper page + add table to schedule

--- movie_view/views.py
+++ movie_view/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     url = 'http://127.0.0.1:8001/cartelera'
-    data = {'location': 'moravia', 'cinema_id': 490}
+    data = {'cinema_id': request.POST['pelicula']}
     response = requests.post(url, json=data)
+    cinema_name = {'490':'Moravia',
+                 '251':'Cartago',
+                 '509':'Lindora'}
     
     if response.status_code == 200:
         peliculas = response.json()  # Asumiendo que la respuesta es un JSON con los datos esperados
@@ -12,4 +15,4 @@ def home(request):
         peliculas = []  # Asegura que 'peliculas' esté definido aunque la petición falle
         print("Error al obtener datos:", response.status_code, response.text)
     
-    return render(request, 'home.html', {'peliculas': peliculas})
+    return render(request, 'home.html', {'peliculas': peliculas, 'cinema_name': cinema_name[request.POST['pelicula']]})
